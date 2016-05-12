00d30fdb16e64be9d1c2a5c1651f7eba2a98c06e
Ricky Cook <mail@thatpanda.com>
2016-05-12 17:46:48 +1000

    200 and 404 for log requests

--- dockci/logserve/http.py
+++ dockci/logserve/http.py
@@ -1,6 +1,8 @@
 """ Setup and run the DockCI log server API server """
 import asyncio
 
+import py
+
 from aiohttp import web
 
 from .util import run_wrapper
@@ -10,8 +12,9 @@ APP = web.Application()
 
 
 @run_wrapper('http')
-def run(*_):
+def run(logger, *_):
     """ Run the HTTP API server """
+    APP.logger = logger
     web.run_app(APP)
 
 
@@ -22,3 +25,34 @@ def handle_health(_):
 
 
 APP.router.add_route('GET', '/_healthz', handle_health)
+
+
+@asyncio.coroutine
+def handle_log(request):
+    # import logging
+    # logging.info(request.GET['a'])
+    params = request.match_info
+    log_path = py.path.local('data').join(
+        params['project_slug'],
+        params['job_slug'],
+        params['stage_slug'],
+    )
+    if not log_path.check():
+        APP.logger.warning(
+            'Log for project %s job %s stage %s not found',
+            params['project_slug'], params['job_slug'], params['stage_slug'],
+        )
+        return web.Response(status=404)
+
+    APP.logger.info(
+        'Log for project %s job %s stage %s found',
+        params['project_slug'], params['job_slug'], params['stage_slug'],
+    )
+    return web.Response(body="Found".encode(), status=200)
+
+
+APP.router.add_route(
+    'GET',
+    '/projects/{project_slug}/jobs/{job_slug}/log_init/{stage_slug}',
+    handle_log,
+)
